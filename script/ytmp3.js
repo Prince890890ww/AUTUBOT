f095395e27d9d18834061f34a041e052ddd66d09
makabugto123 <139425756+makabugto123@users.noreply.github.com>
2024-12-28 15:55:09 +0800

    Update ytmp3.js

--- script/ytmp3.js
+++ script/ytmp3.js
@@ -16,77 +16,82 @@ module.exports.config = {
 
 module.exports.run = async function({ api, event, args }) {
   const { threadID, messageID } = event;
-  
+
   const input = args.join(' ');
 
   if (!input) {
     api.sendMessage(
-      "[ YTMP3 ]\n\nPlease provide a song title after 'ytmp3'. Example: 'ytmp3 love me for who i am'",
-      event.threadID,
-      event.messageID
+      "[ YTMP3 ]\n\nPlease provide a song title after 'ytmp3'. Example: 'ytmp3 love me for who I am'",
+      threadID,
+      messageID
     );
     return;
   }
 
   api.sendMessage(
-    "Sending Music is sending, please wait...",
+    "Music is being sent, please wait...",
     threadID,
     (err, info) => {
       if (err) return;
-      
+
       axios
-        .post("https://kaiz-apis.gleeze.com/api/ytsearch?q=${encodeURIComponent(input)}")
+        .post(`https://kaiz-apis.gleeze.com/api/ytsearch?q=${encodeURIComponent(input)}`)
         .then(async ({ data }) => {
           const info = data.items[0];
           const title = info.title;
           const url = info.url;
 
-      axios
-        .post("https://kaiz-ytmp4-downloader.vercel.app/ytmp4?url=${url}")
-        .then(async ({ data }) => {
-         // const title = data.title;
-          const dlink = data.download_url;
-          //const nickname = data.nickname;
+          axios
+            .post(`https://kaiz-ytmp4-downloader.vercel.app/ytmp4?url=${url}`)
+            .then(async ({ data }) => {
+              const dlink = data.download_url;
 
-          const audioPath = path.resolve(__dirname, 'song.mp3');
-          const writer = fs.createWriteStream(audioPath);
+              const audioPath = path.resolve(__dirname, 'song.mp3');
+              const writer = fs.createWriteStream(audioPath);
 
-          const responseStream = await axios({
-            url: dlink,
-            method: 'GET',
-            responseType: 'stream',
-          });
+              const responseStream = await axios({
+                url: dlink,
+                method: 'GET',
+                responseType: 'stream',
+              });
 
-          responseStream.data.pipe(writer);
+              responseStream.data.pipe(writer);
 
-          writer.on('finish', () => {
-            api.sendMessage(
-              {
-                body: `Title: ${username}\n`,
-                attachment: fs.createReadStream(audioPath),
-              },
-              threadID,
-              () => {
-                fs.unlinkSync(videoPath);
+              writer.on('finish', () => {
+                api.sendMessage(
+                  {
+                    body: `Title: ${title}\n`,
+                    attachment: fs.createReadStream(audioPath),
+                  },
+                  threadID,
+                  () => {
+                    fs.unlinkSync(audioPath);
+                    api.editMessage(
+                      "[ YTMP3 ]\n\nMusic sent successfully!",
+                      info.messageID
+                    );
+                  },
+                  messageID
+                );
+              });
+
+              writer.on('error', () => {
                 api.editMessage(
-                  "[ YTMP3 ]\n\nVideo sent successfully!",
+                  "[ YTMP3 ]\n\nAn error occurred while processing the audio.",
                   info.messageID
                 );
-              },
-              messageID
-            );
-          });
-
-          writer.on('error', () => {
-            api.editMessage(
-              "[ YTMP3 ]\n\nAn error occurred while processing the video.",
-              info.messageID
-            );
-          });
-       } })
+              });
+            })
+            .catch(() => {
+              api.editMessage(
+                "[ Youtube Downloader ]\n\nError fetching download API!",
+                info.messageID
+              );
+            });
+        })
         .catch(() => {
           api.editMessage(
-            "[ Youtube Downloader ]\n\nError fetching girl edit API!",
+            "[ Youtube Downloader ]\n\nError fetching search API!",
             info.messageID
           );
         });
